d9a23fdc1a7d8f1f62a6211543eb168cb2b37cdc
Santiago Varela Daza <svarela03@uan.edu.co>
2022-04-14 18:28:40 -0500

    Initial commit - Santiago Varela

---
+++ src/main/java/com/uan/pasajesonline/PasajesOnLineApplication.java
@@ -0,0 +1,13 @@
+package com.uan.pasajesonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasajesOnLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasajesOnLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uan/pasajesonline/PasajesOnLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uan.pasajesonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasajesOnLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
