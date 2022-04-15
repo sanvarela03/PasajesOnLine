6644e39cb0016846c1a7f49fb4abe8d6058bb12f
Santiago Varela Daza <svarela03@uan.edu.co>
2022-04-14 19:08:02 -0500

    Commit de prueba - pruebas

--- src/main/java/com/uan/pasajesonline/PasajesOnLineApplication.java
+++ src/main/java/com/uan/pasajesonline/PasajesOnLineApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PasajesOnLineApplication {
 //holaa
+
+    //Pruebas branch
+
+
+    //Mod 1 pruebas
     public static void main(String[] args) {
         SpringApplication.run(PasajesOnLineApplication.class, args);
     }
