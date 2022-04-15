7ac3424e90fff23861904c081e78ed76f562cb39
Sercroft <fugitivesebas31@hotmail.es>
2022-04-14 19:18:04 -0500

    Prueba rama NADA

--- src/main/java/com/uan/pasajesonline/PasajesOnLineApplication.java
+++ src/main/java/com/uan/pasajesonline/PasajesOnLineApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PasajesOnLineApplication {
-//holaa
+//PRUEBAAAA
     public static void main(String[] args) {
         SpringApplication.run(PasajesOnLineApplication.class, args);
     }
